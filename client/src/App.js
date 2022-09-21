3a8b510c72bae568a45b5c53ee88cd0cf6c92bd7
ej <103414353+ej-uses-git@users.noreply.github.com>
2022-09-21 20:15:59 +0300

    Create Context Menu

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,10 @@ function App() {
           <Route index element={<Navigate to="joen" />} />
           <Route path="joen">
             <Route index element={<Navigate to="main" />} />
-            <Route path=":folderId" element={<Folder />} />
+            <Route path=":folderId" element={<Folder />}>
+              <Route path=":file/info" element={<div>Info</div>}/>
+              <Route path=":file/display" element={<div>Show</div>}/>
+            </Route>
           </Route>
         </Route>
         <Route path="/" element={<Navigate to="/users" />} />
--- client/src/pages/Folder.js
+++ client/src/pages/Folder.js
@@ -1,16 +1,62 @@
 import { formatNames } from "../utilities/folder-utils";
 import { getFetch } from "../utilities/fetch-utils";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { Outlet, useNavigate, useParams } from "react-router-dom";
+import ContextMenu from "../components/ContextMenu";
 
 const URL = "http://10.0.0.55:3000/users/joen";
 
 function Folder() {
+  const navigate = useNavigate();
   const { folderId } = useParams();
+
   const [fileData, setFileData] = useState([]);
+  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
+  const [showMenu, setShowMenu] = useState(false);
+  const [hasContext, setHasContext] = useState();
+
+  const handleContextMenu = useCallback(
+    (event) => {
+      event.preventDefault();
+      setAnchorPoint({ x: event.pageX, y: event.pageY });
+      setShowMenu(true);
+      setHasContext(event.target.title);
+    },
+    [setAnchorPoint, setShowMenu, setHasContext]
+  );
+
+  const handleOptionSelect = (event) => {
+    const { title } = event.target;
+    switch (title) {
+      case "info":
+        return navigate(`${hasContext}/info`);
+      case "show":
+        return navigate(`${hasContext}/display`);
+      case "delete":
+        //
+        break;
+      case "copy":
+        //
+        break;
+    }
+  };
+
+  const handleClick = useCallback(
+    () => (showMenu ? setShowMenu(false) : null),
+    [showMenu]
+  );
+
+  useEffect(() => {
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  });
+
   useEffect(() => {
     (async () => {
-      const data = await getFetch(URL);
+      // const data = await getFetch(URL);
+      const data = ["app.js", "examples", "b.txt"];
       setFileData(data);
     })();
   }, []);
@@ -18,6 +64,16 @@ function Folder() {
   const fileNames = formatNames(fileData);
   return (
     <div className="folder">
+      {showMenu && (
+        <ContextMenu
+          onOptionSelect={handleOptionSelect}
+          anchorPoint={anchorPoint}
+          onContext={handleContextMenu}
+        />
+      )}
+
+      {}
+
       <table className="folder-contents">
         <thead className="folder-headings">
           <tr>
@@ -26,15 +82,23 @@ function Folder() {
           </tr>
         </thead>
         <tbody className="folder-files">
-          {fileNames.map((file) => (
+          {fileNames.map((file, i) => (
             <tr key={file} className="folder-file">
               {file.map((text) => (
-                <td key={file + "--" + text}>{text}</td>
+                <td
+                  title={fileData[i]}
+                  key={file + "--" + text}
+                  onContextMenu={handleContextMenu}
+                >
+                  {text}
+                </td>
               ))}
             </tr>
           ))}
         </tbody>
       </table>
+
+      <Outlet />
     </div>
   );
 }
